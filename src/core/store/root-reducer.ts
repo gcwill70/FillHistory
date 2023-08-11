85b4a5a8767f5a12586830df574615b95245da35
gcwill70 <gcwill70@gmail.com>
2023-08-11 13:30:30 -0400

    add history reducer

--- src/core/store/root-reducer.ts
+++ src/core/store/root-reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { ActionType, EmptyAction } from "typesafe-actions";
 import { commandSlice, tabsSlice } from "./slices";
+import { historySlice } from "./slices/history-slice";
 
 export const rootReducer = combineReducers({
   command: commandSlice.reducer,
   tabs: tabsSlice.reducer,
+  history: historySlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -12,4 +14,5 @@ export type RootAction =
   | EmptyAction<"init/start">
   | EmptyAction<"init/done">
   | ActionType<typeof tabsSlice.actions>
-  | ActionType<typeof commandSlice.actions>;
+  | ActionType<typeof commandSlice.actions>
+  | ActionType<typeof historySlice.actions>;
