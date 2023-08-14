9f49fe680881919424b607a12968dd2849b9bd2f
gcwill70 <gcwill70@gmail.com>
2023-08-14 18:49:58 -0400

    move window styling to history_window

--- src/history/history_view.tsx
+++ src/history/history_view.tsx
@@ -3,29 +3,6 @@ import { useSelector } from "react-redux";
 import { RootState } from "../core";
 import { HistoryItem } from "./model/history_item";
 
-const overlayStyle: React.CSSProperties = {
-  position: "fixed",
-  top: "0",
-  left: "0",
-  width: "100%",
-  height: "100%",
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
-  zIndex: "9999",
-};
-
-const windowStyle: React.CSSProperties = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  backgroundColor: "white",
-  padding: "20px",
-  borderRadius: "5px",
-  width: "400px",
-  maxHeight: "400px",
-  overflowY: "auto",
-};
-
 const resultsListStyle: React.CSSProperties = {
   listStyle: "none",
   padding: "0",
@@ -45,33 +22,27 @@ export default function HistoryView() {
   const { id } = useSelector((state: RootState) => state.activeElement);
 
   return (
-    <div id="history-window" style={overlayStyle}>
-      <div id="window" style={windowStyle}>
-        <ul id="results-list" style={resultsListStyle}>
-          {items.map((item: HistoryItem, i: number) => {
-            return (
-              <li
-                style={listItemStyle}
-                key={`results-list-${i}`}
-                onClick={() => {
-                  if (id) {
-                    let activeElement = document.getElementById(
-                      id
-                    ) as HTMLElement;
-                    if (activeElement.nodeName === "INPUT") {
-                      (activeElement as HTMLInputElement).value = item.url!;
-                    } else if (activeElement.nodeName === "TEXTAREA") {
-                      (activeElement as HTMLTextAreaElement).value = item.url!;
-                    }
-                  }
-                }}
-              >
-                {item.url}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+    <ul id="results-list" style={resultsListStyle}>
+      {items.map((item: HistoryItem, i: number) => {
+        return (
+          <li
+            style={listItemStyle}
+            key={`results-list-${i}`}
+            onClick={() => {
+              if (id) {
+                let activeElement = document.getElementById(id) as HTMLElement;
+                if (activeElement.nodeName === "INPUT") {
+                  (activeElement as HTMLInputElement).value = item.url!;
+                } else if (activeElement.nodeName === "TEXTAREA") {
+                  (activeElement as HTMLTextAreaElement).value = item.url!;
+                }
+              }
+            }}
+          >
+            {item.url}
+          </li>
+        );
+      })}
+    </ul>
   );
 }
--- src/history/history_window.tsx
+++ src/history/history_window.tsx
@@ -3,14 +3,39 @@ import { RootState } from "../core";
 import HistoryForm from "./history_form";
 import HistoryView from "./history_view";
 
+const overlayStyle: React.CSSProperties = {
+  position: "fixed",
+  top: "0",
+  left: "0",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  zIndex: "9999",
+};
+
+const windowStyle: React.CSSProperties = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "white",
+  padding: "20px",
+  borderRadius: "5px",
+  width: "400px",
+  maxHeight: "400px",
+  overflowY: "auto",
+};
+
 export default function HistoryWindow() {
   const show = useSelector((state: RootState) => state.history.window.show);
   return (
     <div>
       {show && (
-        <div>
-          <HistoryForm />
-          <HistoryView />
+        <div style={overlayStyle}>
+          <div style={windowStyle}>
+            <HistoryForm />
+            <HistoryView />
+          </div>
         </div>
       )}
     </div>
