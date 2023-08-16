1f1a92ab4449b6413db815757ac4b5e7f74ebe8a
gcwill70 <gcwill70@gmail.com>
2023-08-16 15:55:22 -0400

    clear results on tab change

--- src/history/history-controller.ts
+++ src/history/history-controller.ts
@@ -1,6 +1,7 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
 import HistoryApiChrome from "./api/history_api_chrome";
 import { historySlice } from "../core/store/slices/history-slice";
+import { tabsSlice } from "../core/store/slices/tabs-slice";
 
 const historyController = createListenerMiddleware();
 
@@ -10,6 +11,18 @@ historyController.startListening({
     try {
       const query = action.payload;
       const results = await new HistoryApiChrome().search({ ...query });
+      api.dispatch(historySlice.actions.queryDone(results));
+    } catch (e) {
+      api.dispatch(historySlice.actions.queryError());
+    }
+  },
+});
+
+historyController.startListening({
+  actionCreator: tabsSlice.actions.setTab,
+  effect: async (action, api) => {
+    try {
+      const results = await new HistoryApiChrome().search({ text: "" });
       api.dispatch(historySlice.actions.queryDone(results));
     } catch (e) {
       api.dispatch(historySlice.actions.queryError());
