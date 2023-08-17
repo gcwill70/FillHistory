c8650c1962f234b52679b64ee7bf97fb20fad24e
gcwill70 <gcwill70@gmail.com>
2023-08-17 17:16:55 -0400

    update to use underlying class

--- src/history/api/history_api_chrome.ts
+++ src/history/api/history_api_chrome.ts
@@ -2,7 +2,7 @@ import { HistoryItem } from "../model/history_item";
 import { HistoryQuery } from "../model/history_query";
 import HistoryApi from "./history_api";
 
-export default class HistoryApiChrome implements HistoryApi {
+export default class HistoryApiChrome extends HistoryApi {
   async search(query: HistoryQuery): Promise<HistoryItem[]> {
     const results = await chrome.history.search({ ...query });
     return results.map((item) => ({ ...item } as HistoryItem));
