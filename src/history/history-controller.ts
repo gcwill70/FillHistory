a60d2d7257e376f14a4945ad57bfdfcedee18101
gcwill70 <gcwill70@gmail.com>
2023-08-17 17:17:32 -0400

    create single api instance

--- src/history/history-controller.ts
+++ src/history/history-controller.ts
@@ -3,28 +3,29 @@ import { historySlice } from "../core/store/slices/history-slice";
 import HistoryApiChrome from "./api/history_api_chrome";
 
 const historyController = createListenerMiddleware();
+const historyApi = new HistoryApiChrome();
 
 historyController.startListening({
   actionCreator: historySlice.actions.queryStart,
-  effect: async (action, api) => {
+  effect: async (action, listenerApi) => {
     try {
       const query = action.payload;
-      const results = await new HistoryApiChrome().search({ ...query });
-      api.dispatch(historySlice.actions.queryDone(results));
+      const results = await historyApi.search({ ...query });
+      listenerApi.dispatch(historySlice.actions.queryDone(results));
     } catch (e) {
-      api.dispatch(historySlice.actions.queryError());
+      listenerApi.dispatch(historySlice.actions.queryError());
     }
   },
 });
 
 historyController.startListening({
   actionCreator: historySlice.actions.hideWindow,
-  effect: async (action, api) => {
+  effect: async (action, listenerApi) => {
     try {
-      const results = await new HistoryApiChrome().search({ text: "" });
-      api.dispatch(historySlice.actions.queryDone(results));
+      const results = await historyApi.search({ text: "" });
+      listenerApi.dispatch(historySlice.actions.queryDone(results));
     } catch (e) {
-      api.dispatch(historySlice.actions.queryError());
+      listenerApi.dispatch(historySlice.actions.queryError());
     }
   },
 });
