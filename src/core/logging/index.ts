fdde847dd289d468013ccebbd31b9843fbe96721
gcwill70 <gcwill70@gmail.com>
2023-08-18 11:45:23 -0400

    add logging

---
+++ src/core/logging/index.ts
@@ -0,0 +1,6 @@
+import debug from "debug";
+
+export const log = debug("FillHistory:log");
+export const debugLog = debug("FillHistory:debug"); // Create a debug level
+export const warn = debug("FillHistory:warn");
+export const error = debug("FillHistory:error");
