66a09e5752cd28db50b18512ba88ed0b2cf61724
gcwill70 <gcwill70@gmail.com>
2023-08-22 13:24:23 -0400

    select items with arrow keys

--- src/extension/content/listeners/index.ts
+++ src/extension/content/listeners/index.ts
@@ -1,6 +1,8 @@
 import { createStore } from "../../../core";
 import { activeElementListener } from "./active-element-listener";
+import { itemSelectListener } from "./item-select-listener";
 
 export function subscribeListeners(store: ReturnType<typeof createStore>) {
   activeElementListener(store);
+  itemSelectListener(store);
 }
