beacea0e9f6dcdc527a2bc85929aa1b87abf18ab
gcwill70 <gcwill70@gmail.com>
2023-09-08 15:32:55 -0400

    remove debug message

--- src/extension/background/listeners/index.ts
+++ src/extension/background/listeners/index.ts
@@ -1,13 +1,17 @@
 import { createStore } from "../../../core";
 import { commandsListener } from "./commands-listener";
+import { contextmenuListener } from "./contextmenu-listener";
 import { runtimeListener } from "./runtime-listener";
 import { setTabListener } from "./set-tab-listener";
 
 export * from "./commands-listener";
+export * from "./contextmenu-listener";
+export * from "./runtime-listener";
 export * from "./set-tab-listener";
 
 export function subscribeListeners(store: ReturnType<typeof createStore>) {
   setTabListener(store);
   commandsListener(store);
   runtimeListener(store);
+  contextmenuListener(store);
 }
