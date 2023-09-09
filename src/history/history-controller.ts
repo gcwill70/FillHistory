07e7fe7452c2fb7026fe2f69c6711a5d8c82a4e1
gcwill70 <gcwill70@gmail.com>
2023-09-09 12:58:12 -0400

    make queryStart non-blocking

--- src/history/history-controller.ts
+++ src/history/history-controller.ts
@@ -7,26 +7,29 @@ import HistoryRepo from "./repo/history_repo";
 const historyController = createListenerMiddleware<RootState>();
 const repo = new HistoryRepo(new HistoryApiChrome());
 
-// search start
+// search
 historyController.startListening({
   actionCreator: historySlice.actions.queryStart,
-  effect: async (action, api) => {
-    try {
-      const query = action.payload;
-      let results = await repo.search({ ...query });
-      results = await repo.filter(results);
-      api.dispatch(historySlice.actions.queryDone(results));
-      api.dispatch(historySlice.actions.selectionReset());
-    } catch (e) {
-      api.dispatch(historySlice.actions.queryError());
-    }
+  effect: (action, api) => {
+    repo.search({ ...action.payload }).then(
+      (results) => {
+        repo.filter(results).then(
+          (results) => {
+            api.dispatch(historySlice.actions.queryDone(results));
+            api.dispatch(historySlice.actions.selectionReset());
+          },
+          (reason) => api.dispatch(historySlice.actions.queryError())
+        );
+      },
+      (reason) => api.dispatch(historySlice.actions.queryError())
+    );
   },
 });
 
 // reset handler
 historyController.startListening({
   actionCreator: historySlice.actions.reset,
-  effect: async (action, api) => {
+  effect: (action, api) => {
     api.dispatch(historySlice.actions.windowHide());
     api.dispatch(
       historySlice.actions.queryStart({
