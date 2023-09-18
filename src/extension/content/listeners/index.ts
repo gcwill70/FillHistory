5dd2e26ce517302b1fafb32b2bf49219d6fb5d8d
gcwill70 <gcwill70@gmail.com>
2023-09-18 14:50:58 -0400

    add runtime listener to content script

--- src/extension/content/listeners/index.ts
+++ src/extension/content/listeners/index.ts
@@ -1,8 +1,10 @@
 import { createStore } from "../../../core";
 import { activeElementListener } from "./active_element_listener";
 import { keydownListener } from "./keydown_listener";
+import { runtimeListener } from "./runtime_listener";
 
 export function subscribeListeners(store: ReturnType<typeof createStore>) {
+  runtimeListener(store);
   activeElementListener(store);
   keydownListener(store);
 }
