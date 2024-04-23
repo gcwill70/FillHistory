a08bb0745fdc4f8645ad76fdff7ff993cc2317f3
gcwill70 <gcwill70@gmail.com>
2024-04-22 22:14:20 -0400

    create separate background store

---
+++ src/extension/background/store/create_store.ts
@@ -0,0 +1,30 @@
+import { Middleware, configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import commandsController from "../../../commands/commands_controller";
+import paymentController from "../../../payment/payment_controller";
+import premiumController from "../../../premium/premium_controller";
+import searchController from "../../../search/search_controller";
+import tabController from "../../../tabs/tab_controller";
+import { BackgroundAction, BackgroundState, backgroundReducer } from "./reducer";
+
+export function createStore(
+  preloadedState: BackgroundState = backgroundReducer(undefined, { type: "lifecycle" })
+) {
+  const store = configureStore<BackgroundState, BackgroundAction, Middleware[]>({
+    reducer: backgroundReducer,
+    preloadedState: preloadedState,
+    middleware: (def) =>
+      def().concat(
+        logger,
+        searchController.middleware,
+        tabController.middleware,
+        commandsController.middleware,
+        paymentController.middleware,
+        premiumController.middleware
+      ),
+  });
+
+  return store;
+}
+
+export type Store = ReturnType<typeof createStore>;
---
+++ src/extension/background/store/reducer.ts
@@ -0,0 +1,27 @@
+import { AnyAction, combineReducers } from "@reduxjs/toolkit";
+import { ActionType } from "typesafe-actions";
+import { commandsSlice } from "../../../commands/commands_slice";
+import { lifecycleSlice } from "../../../lifecycle/lifecycle_slice";
+import { paymentSlice } from "../../../payment/payment_slice";
+import { premiumSlice } from "../../../premium/premium_slice";
+import { searchSlice } from "../../../search/search_slice";
+import { tabsSlice } from "../../../tabs/tabs_slice";
+
+export const backgroundReducer = combineReducers({
+  commands: commandsSlice.reducer,
+  search: searchSlice.reducer,
+  lifecycle: lifecycleSlice.reducer,
+  tabs: tabsSlice.reducer,
+  payment: paymentSlice.reducer,
+  premium: premiumSlice.reducer,
+});
+
+export type BackgroundState = ReturnType<typeof backgroundReducer>;
+export type BackgroundAction =
+  | AnyAction
+  | ActionType<typeof commandsSlice.actions>
+  | ActionType<typeof searchSlice.actions>
+  | ActionType<typeof lifecycleSlice.actions>
+  | ActionType<typeof paymentSlice.actions>
+  | ActionType<typeof premiumSlice.actions>
+  | ActionType<typeof tabsSlice.actions>;
