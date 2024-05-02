3fb5e2b556b760adfa2fd2817ececb93ff333d6d
gcwill70 <gcwill70@gmail.com>
2024-05-02 00:01:32 -0400

    send message in commands/command event

--- src/extension/background/listeners/commands_listener.ts
+++ src/extension/background/listeners/commands_listener.ts
@@ -1,12 +1,14 @@
 import { commandsSlice } from "../../../commands-background/commands_slice";
-import { searchSlice } from "../../../search/search_slice";
+import { messageSlice } from "../../../message/message_slice";
 import { createStore } from "../store/create_store";
 
 export function commandsListener(store: ReturnType<typeof createStore>) {
   chrome.commands.onCommand.addListener(async function(command) {
     store.dispatch(commandsSlice.actions.command(command));
-    if (command === "search") {
-      store.dispatch(searchSlice.actions.window());
-    }
+    store.dispatch(
+      messageSlice.actions.sendMessage({
+        "commands/command": command,
+      })
+    );
   });
 }
