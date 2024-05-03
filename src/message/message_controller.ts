856c5c1046211a3454bad6c0a21239f0e0291216
Geoff Cramer <gcwill70@gmail.com>
2024-05-03 10:46:39 -0400

    clean up messagecontroller

--- src/message/message_controller.ts
+++ src/message/message_controller.ts
@@ -1,29 +1,34 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
-import { messageSlice } from "./message_slice";
 import { lifecycleSlice } from "../lifecycle-background/lifecycle_slice";
+import { messageSlice } from "./message_slice";
 
 const messageController = createListenerMiddleware();
 
-// sendMessage
+// init
+messageController.startListening({
+  actionCreator: lifecycleSlice.actions.initStart,
+  effect: async (action, api) => {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      console.debug(
+        "message.onMessage: ",
+        JSON.stringify(message),
+        JSON.stringify(sender)
+      );
+      api.dispatch(messageSlice.actions.onMessage({ message, sender }));
+    });
+  },
+});
+
+// sendMessage handler
 messageController.startListening({
   actionCreator: messageSlice.actions.sendMessage,
   effect: async (action, api) => {
+    console.debug("message.sendMessage: ", JSON.stringify(action));
     try {
       chrome.runtime.sendMessage({ ...action.payload });
     } catch (e) {
       api.dispatch(messageSlice.actions.messageError());
     }
-  },
-});
-
-// onMessage listener
-messageController.startListening({
-  actionCreator: lifecycleSlice.actions.initStart,
-  effect: async (action, api) => {
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      console.log("onMessage", request, sender);
-      api.dispatch(messageSlice.actions.onMessage({ request, sender }));
-    });
   },
 });
 
