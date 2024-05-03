61e46de8d6ea6693234282bd2efdcf7d57c1907d
Geoff Cramer <gcwill70@gmail.com>
2024-05-03 10:50:30 -0400

    remove commands listener

--- src/extension/background/listeners/index.ts
+++ src/extension/background/listeners/index.ts
@@ -1,5 +1,4 @@
 import { createStore } from "../store/create_store";
-import { commandsListener } from "./commands_listener";
 import { contextmenuListener } from "./contextmenu_listener";
 import { runtimeListener } from "./runtime_listener";
 import { storageListener } from "./storage_listener";
@@ -8,7 +7,6 @@ import { setTabListener } from "./tab_listener";
 export function subscribeListeners(store: ReturnType<typeof createStore>) {
   storageListener(store);
   setTabListener(store);
-  commandsListener(store);
   runtimeListener(store);
   contextmenuListener(store);
 }
