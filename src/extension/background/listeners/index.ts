ced2bbd477a71494f45a094b3459b56efe720fca
gcwill70 <gcwill70@gmail.com>
2024-05-05 13:09:44 -0400

    remove storage_listener

--- src/extension/background/listeners/index.ts
+++ src/extension/background/listeners/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from "../store";
 import { runtimeListener } from "./runtime_listener";
-import { storageListener } from "./storage_listener";
 import { setTabListener } from "./tab_listener";
 
 export function subscribeListeners(store: ReturnType<typeof createStore>) {
-  storageListener(store);
   setTabListener(store);
   runtimeListener(store);
 }
