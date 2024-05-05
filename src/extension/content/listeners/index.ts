edcbbd8bcec91c2b44eb52060853133499b3655a
gcwill70 <gcwill70@gmail.com>
2024-05-05 13:22:27 -0400

    remove content runtime_listener

--- src/extension/content/listeners/index.ts
+++ src/extension/content/listeners/index.ts
@@ -1,8 +1,6 @@
 import { createStore } from "../store";
 import { activeElementListener } from "./active_element_listener";
-import { runtimeListener } from "./runtime_listener";
 
 export function subscribeListeners(store: ReturnType<typeof createStore>) {
-  runtimeListener(store);
   activeElementListener(store);
 }
