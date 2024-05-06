1df67d00784979398af7cdf517947994fa041e7b
gcwill70 <gcwill70@gmail.com>
2024-05-05 20:09:55 -0400

    remove webext-redux from popup

--- src/extension/popup/index.tsx
+++ src/extension/popup/index.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { Store } from "webext-redux";
 import App from "./App";
 
 (async () => {
-  const store = new Store();
-  await store.ready();
-
   // create UI root
   const root = document.createElement("div");
   createRoot(root).render(
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   );
 
   // inject into page
